dafa5173e82e18352143aea55aa6734407f23057
Harvey Wright <thephoenixproject208@gmail.com>
2019-03-14 16:26:26 +0000

    Initial commit

---
+++ BetterAdmin.cs
@@ -0,0 +1,76 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace BetterAdmin
+{
+    [PluginDetails(
+        author = "Phoenix",
+        name = "BetterAdmin",
+        description = "Plugin that improves administation capabilties",
+        id = "phoenix.betteradmin",
+        version = "1.0",
+        SmodMajor = 3,
+        SmodMinor = 3,
+        SmodRevision = 0
+        )]
+    class Betteradmin : Plugin
+    {
+        public int[] itemblacklist { get; private set; }
+        public int[] roleblacklist { get; private set; }
+        public string[] itemblacklistranks { get; private set; }
+        public string[] roleblacklistranks { get; private set; }
+
+        public bool anticamp106 { get; private set; }
+        public bool anticamp079 { get; private set; }
+        public bool anticampnuke { get; private set; }
+
+        public bool idisable { get; private set; }
+        public bool rdisable { get; private set; }
+
+        public override void OnDisable()
+		{
+			this.Info(Details.name + " version " + Details.version + " was disabled");
+		}
+
+		public override void OnEnable()
+		{
+            this.Info(Details.name + " version " + Details.version + " was enabled");
+        }
+
+		public override void Register()
+		{
+            this.AddEventHandlers(new EventHandler(this));
+
+            this.AddConfig(new ConfigSetting("ba_items_blacklist", new[] { 25 }, SettingType.NUMERIC_LIST, true, "List of items that should not be able to be spawned"));
+            this.AddConfig(new ConfigSetting("ba_roles_blacklist", new[] { 14 }, SettingType.NUMERIC_LIST, true, "List of roles that should not be able to be spawned"));
+            this.AddConfig(new ConfigSetting("ba_items_blacklist_ranks", new[] { "owner" }, SettingType.LIST, true, "List of ranks that bypass the item blacklist"));
+            this.AddConfig(new ConfigSetting("ba_roles_blacklist_ranks", new[] { "owner" }, SettingType.LIST, true, "List of ranks that bypass the role blacklist"));
+
+            this.AddConfig(new ConfigSetting("ba_anticamp_106", true, SettingType.BOOL, true, "Locks the 106 chamber doors open upon 106 being recontained"));
+            this.AddConfig(new ConfigSetting("ba_anticamp_079", true, SettingType.BOOL, true, "Locks the 079 chamber doors open upon activating all 5 generators"));
+            this.AddConfig(new ConfigSetting("ba_anticamp_nuke", true, SettingType.BOOL, true, "Locks the nuke surface doors open upon the nuke being canceled"));
+
+            this.AddConfig(new ConfigSetting("ba_item_disable", false, SettingType.BOOL, true, "Disables the BetterAdmin item blocker"));
+            this.AddConfig(new ConfigSetting("ba_role_disable", false, SettingType.BOOL, true, "Disables the BetterAdmin role blocker"));
+        }
+
+        public void RefreshConfig()
+        {
+            itemblacklist = GetConfigIntList("ba_items_blacklist");
+            roleblacklist = GetConfigIntList("ba_roles_blacklist");
+            itemblacklistranks = GetConfigList("ba_items_blacklist_ranks");
+            roleblacklistranks = GetConfigList("ba_roles_blacklist_ranks");
+
+            anticamp106 = GetConfigBool("ba_anticamp_106");
+            anticamp079 = GetConfigBool("ba_anticamp_079");
+            anticampnuke = GetConfigBool("ba_anticamp_nuke");
+
+            idisable = GetConfigBool("ba_item_disable");
+            rdisable = GetConfigBool("ba_role_disable");
+        }
+	}
+}
---
+++ EventHandler.cs
@@ -0,0 +1,110 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterAdmin
+{
+    class EventHandler : IEventHandlerAdminQuery, IEventHandlerWarheadStopCountdown, IEventHandlerWarheadStartCountdown, IEventHandlerContain106, IEventHandlerGeneratorFinish, IEventHandlerWaitingForPlayers
+    {
+        private readonly Betteradmin plugin;
+
+        //public string CrtItem;
+        public string ARank;
+        public string AQuery;
+        public int generatorPowerups = 0;
+        public int itemrole;
+
+        public EventHandler(Betteradmin plugin) => this.plugin = plugin; //Expression bodies can also be used
+
+        //public void OnShoot(PlayerShootEvent ev)
+        //{
+        //    if (ev.Player.TeamRole.Role == Role.TUTORIAL)
+        //    {
+        //        ev.Player.ThrowGrenade(0, true, Smod2.API.Vector.Zero, false, ev.Player.GetPosition(), true, 10);
+        //    }
+        //}
+
+        public void OnAdminQuery(AdminQueryEvent ev)
+        {
+            AQuery = ev.Query.ToLower();
+            string[] AAQuery = AQuery.Split(' ');
+            ARank = ev.Admin.GetRankName();
+            if (AAQuery[0].Contains("give") && plugin.idisable == false)
+            {
+                int.TryParse(AAQuery[2], out itemrole);
+                plugin.Info(itemrole.ToString());
+                if (plugin.itemblacklist.Contains(itemrole) && !plugin.itemblacklistranks.Contains(ARank))
+                {
+                    ev.Handled = true;
+                    ev.Successful = false;
+                }
+            }
+            else if (AAQuery[0].Contains("forceclass") && plugin.rdisable == false)
+            {
+                int.TryParse(AAQuery[2], out itemrole);
+                if (plugin.roleblacklist.Contains(itemrole) && !plugin.roleblacklistranks.Contains(ARank))
+                {
+                    ev.Handled = true;
+                    ev.Successful = false;
+                }
+            }
+        }
+
+        public void OnContain106(PlayerContain106Event ev)
+        {
+            if (plugin.anticamp106 == true)
+            {
+                List<Smod2.API.Door> doors = plugin.Server.Map.GetDoors();
+                doors.First(x => x.Name == "106_PRIMARY").Locked = true;
+                doors.First(x => x.Name == "106_SECONDARY").Locked = true;
+                doors.First(x => x.Name == "106_BOTTOM").Locked = true;
+                doors.First(x => x.Name == "106_PRIMARY").Open = true;
+                doors.First(x => x.Name == "106_SECONDARY").Open = true;
+                doors.First(x => x.Name == "106_BOTTOM").Open = true;
+            }
+        }
+
+        public void OnGeneratorFinish(GeneratorFinishEvent ev)
+        {
+            generatorPowerups++; ;
+
+            if (generatorPowerups == 5 && plugin.anticamp079 == true)
+            {
+                List<Smod2.API.Door> doors = plugin.Server.Map.GetDoors();
+                doors.First(x => x.Name == "079_FIRST").Locked = true;
+                doors.First(x => x.Name == "079_SECOND").Locked = true;
+                doors.First(x => x.Name == "079_FIRST").Open = true;
+                doors.First(x => x.Name == "079_SECOND").Open = true;
+            }
+        }
+
+        public void OnStartCountdown(WarheadStartEvent ev)
+        {
+            if (plugin.anticampnuke == true)
+            {
+                List<Smod2.API.Door> doors = plugin.Server.Map.GetDoors();
+                doors.First(x => x.Name == "NUKE_SURFACE").Locked = false;
+                doors.First(x => x.Name == "NUKE_SURFACE").Open = false;
+            }
+        }
+
+        public void OnStopCountdown(WarheadStopEvent ev)
+        {
+            if (plugin.anticampnuke == true)
+            {
+                List<Smod2.API.Door> doors = plugin.Server.Map.GetDoors();
+                doors.First(x => x.Name == "NUKE_SURFACE").Locked = true;
+                doors.First(x => x.Name == "NUKE_SURFACE").Open = true;
+            }
+        }
+
+        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+            plugin.RefreshConfig();
+            generatorPowerups = 0;
+        }
+    }
+}
